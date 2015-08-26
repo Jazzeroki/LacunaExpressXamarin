4db09faa008c64e05e5a56431b4a99729b4cdcd3
RedOrion <redorion0@icloud.com>
2015-08-26 19:49:39 -0400

    iOS now works - no useful GUI though

--- LacunaExpress/LacunaExpress.iOS/Data/IsolatedStorageIOS.cs
+++ LacunaExpress/LacunaExpress.iOS/Data/IsolatedStorageIOS.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using LacunaExpress.Data;
 using LacunaExpress.iOS.Data;
 using Foundation;
+using System.Linq;
 
 [assembly: Dependency(typeof(IsolatedStorageios))]
 namespace LacunaExpress.iOS.Data
