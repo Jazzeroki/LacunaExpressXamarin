8bd4b3df45af9c58bbd8122e05c1598cbe6cfcca
RedOrion <redorion0@icloud.com>
2015-08-28 13:02:02 -0400

    Think thats it.

--- LacunaExpress/LacunaExpress/Pages/About/AboutMain.cs
+++ LacunaExpress/LacunaExpress/Pages/About/AboutMain.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 
+using LacunaExpress.Styles;
+
 using Xamarin.Forms;
 
 namespace LacunaExpress.Pages.About
@@ -15,9 +17,15 @@ namespace LacunaExpress.Pages.About
 		Label questions = new Label();
 		public AboutMain()
 		{
-			top.Text = "Lacuna Express is desinged to be a utility and Mail client for The Lacuna Expanse.  Usage of this client is at the users own risk.  Any proceeds from the sale of this client go to help cover the cost of development tools and to pay for account fees with the different app stores.";
-			donate.Text = "If you would like to donate to help me cover the costs of tools for writing and deploying this app you can send donations to via paypal falcont40@hotmail.com";
-			questions.Text = "If you find any bugs or for suggestions or help send an email to JazzDevStudio@gmail.com or an ingame message to Jazz or TheKi";
+			Entry top = new Label () { Text = "Lacuna Express is desinged to be a utility and Mail client for The Lacuna Expanse.  Usage of this client is at the users own risk.  Any proceeds from the sale of this client go to help cover the cost of development tools and to pay for account fees with the different app stores." };
+			//top.Text = "Lacuna Express is desinged to be a utility and Mail client for The Lacuna Expanse.  Usage of this client is at the users own risk.  Any proceeds from the sale of this client go to help cover the cost of development tools and to pay for account fees with the different app stores.";
+
+			Entry donate = new Label () { Text = "If you would like to donate to help me cover the costs of tools for writing and deploying this app you can send donations to via paypal falcont40@hotmail.com" };
+			//donate.Text = "If you would like to donate to help me cover the costs of tools for writing and deploying this app you can send donations to via paypal falcont40@hotmail.com";
+
+			Entry questions = new Label () { Text = "If you find any bugs or for suggestions or help send an email to JazzDevStudio@gmail.com or an ingame message to Jazz or TheKi" };
+			//questions.Text = "If you find any bugs or for suggestions or help send an email to JazzDevStudio@gmail.com or an ingame message to Jazz or TheKi";
+
 			Content = new StackLayout
 			{
 				Padding = new Thickness(6, 6, 6, 6),
