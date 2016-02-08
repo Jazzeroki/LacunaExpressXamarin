cd5f890359be22ebd0af153fb146b1b12f281835
RedOrion <redorion0@icloud.com>
2016-02-08 14:48:23 -0500

    Add: Mac Support
    Start of Mac Support - Not configured

---
+++ LacunaExpress/LacunaExpress.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace LacunaExpress.Mac
+{
+	static class MainClass
+	{
+		static void Main (string[] args)
+		{
+			NSApplication.Init ();
+			NSApplication.Main (args);
+		}
+	}
+}
